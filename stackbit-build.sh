281ff1e2863921ee49ede5e647798603a6a1fafe
Isaac Volpe <isaac.volpe@gmail.com>
2023-08-21 17:25:26 -0500

    *

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -3,3 +3,14 @@
 set -e
 set -o pipefail
 set -v
+
+curl -s -X POST https://api.stackbit.com/project/5e06e4daae67b10019e5446c/webhook/build/pull > /dev/null
+if [[ -z "${STACKBIT_API_KEY}" ]]; then
+    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
+else
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e06e4daae67b10019e5446c 
+fi
+curl -s -X POST https://api.stackbit.com/project/5e06e4daae67b10019e5446c/webhook/build/ssgbuild > /dev/null
+hugo
+
+curl -s -X POST https://api.stackbit.com/project/5e06e4daae67b10019e5446c/webhook/build/publish > /dev/null
